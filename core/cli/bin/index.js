ce630fc9bc185703aa85f32cbcae07c66a54fc1f
houxingyi <1026043046@qq.com>
2021-06-17 21:01:02 +0800

    重命名

---
+++ core/cli/bin/index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+console.log('hello cli test')
+
+const importLocal = require('import-local');
+
+if (importLocal(__filename)) {
+    require('npmlog').info('cli', '正在使用 h-cli-test 本地版本');
+} else {
+    require('../lib/core')(process.argv.slice(2));
+}
